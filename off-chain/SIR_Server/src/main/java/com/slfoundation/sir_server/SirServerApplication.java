f61ac6e9eb5bf7cf9ce4296d64b91787f4dbda9d
LeeSunMyeong <lsmtjsaud@naver.com>
2024-06-14 16:24:55 +0900

    INIT offchain backend project

---
+++ off-chain/SIR_Server/src/main/java/com/slfoundation/sir_server/SirServerApplication.java
@@ -0,0 +1,13 @@
+package com.slfoundation.sir_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirServerApplication.class, args);
+	}
+
+}
