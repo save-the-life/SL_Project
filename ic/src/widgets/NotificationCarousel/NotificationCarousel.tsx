175886a4ed07d8744e0ba74d50b25341bd8df647
jowooseok <whdntjrsla12@naver.com>
2024-06-14 22:37:57 +0900

    chore: Update npm dependencies for @tailwindcss/line-clamp

--- ic/src/widgets/NotificationCarousel/NotificationCarousel.tsx
+++ ic/src/widgets/NotificationCarousel/NotificationCarousel.tsx
@@ -10,13 +10,28 @@ import Imgs from '@/shared/assets/images';
 import Typography from '@/shared/components/typography';
 import Autoplay from 'embla-carousel-autoplay';
 
+const notifications = [
+  {
+    title: 'Update Notification',
+    message:
+      'We have completed the update to ensure a stable service. Enjoy the improved experience!',
+    image: Imgs.Loudspeaker,
+  },
+  {
+    title: 'Airdrop Event Alert!',
+    message:
+      'Get ready to receive free tokens! Join our special airdrop event from June 20, 2024, to July 20, 2024. Sign up, complete a simple survey, and invite friends to earn even more rewards. Don’t miss this chance to boost your token collection!',
+    image: Imgs.Loudspeaker,
+  },
+];
+
 const NotificationCarousel: React.FC = () => {
   const [api, setApi] = React.useState<CarouselApi>();
   const [current, setCurrent] = React.useState(0);
   const [count, setCount] = React.useState(0);
 
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true }),
+    Autoplay({ delay: 3000, stopOnInteraction: true }),
   );
 
   React.useEffect(() => {
@@ -37,58 +52,33 @@ const NotificationCarousel: React.FC = () => {
       plugins={[plugin.current]}
     >
       <CarouselContent>
-        <CarouselItem>
-          <Card className="rounded-3xl">
-            <div className="flex items-center p-4">
-              <div className="ml-4 flex-1">
-                <Typography.Large>Update Notification</Typography.Large>
-                <Typography.Muted>
-                  We have completed the update to ensure a stable service. Enjoy
-                  the improved experience!
-                </Typography.Muted>
-              </div>
-              <div className="flex flex-col">
-                <img
-                  src={Imgs.Loudspeaker}
-                  alt="Notification"
-                  className="mx-auto w-24 md:w-32"
-                />
-                <div
-                  className="text-white text-sm px-3 rounded-full text-center w-16 self-end"
-                  style={{ backgroundColor: 'rgba(115,115,115,0.8)' }}
-                >
-                  {current} / <span className="opacity-50">{count}</span>
+        {notifications.map((notification, index) => (
+          <CarouselItem key={index}>
+            <Card className="rounded-3xl h-36 ">
+              <div className="flex items-center p-4">
+                <div className="ml-4 flex-1">
+                  <Typography.Large>{notification.title}</Typography.Large>
+                  <Typography.Muted className="line-clamp-3">
+                    {notification.message}
+                  </Typography.Muted>
+                </div>
+                <div className="flex flex-col">
+                  <img
+                    src={notification.image}
+                    alt="Notification"
+                    className="mx-auto w-24 md:w-32"
+                  />
+                  <div
+                    className="text-white text-sm px-3 rounded-full text-center w-16 self-end"
+                    style={{ backgroundColor: 'rgba(115,115,115,0.8)' }}
+                  >
+                    {current} / <span className="opacity-50">{count}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Card>
-        </CarouselItem>
-        <CarouselItem>
-          <Card className="rounded-3xl">
-            <div className="flex items-center p-4">
-              <div className="ml-4 flex-1">
-                <Typography.Large>Update Notification</Typography.Large>
-                <Typography.Muted>
-                  We have completed the update to ensure a stable service. Enjoy
-                  the improved experience!
-                </Typography.Muted>
-              </div>
-              <div className="flex flex-col">
-                <img
-                  src={Imgs.Loudspeaker}
-                  alt="Notification"
-                  className="mx-auto w-24 md:w-32"
-                />
-                <div
-                  className="text-white text-sm px-3 rounded-full text-center w-16 self-end"
-                  style={{ backgroundColor: 'rgba(115,115,115,0.8)' }}
-                >
-                  {current} / <span className="opacity-50">{count}</span>
-                </div>
-              </div>
-            </div>
-          </Card>
-        </CarouselItem>
+            </Card>
+          </CarouselItem>
+        ))}
       </CarouselContent>
     </Carousel>
   );
