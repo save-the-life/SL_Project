9ac9a9feef1b928cefc8b87c1209d78b23bf803a
jowooseok <whdntjrsla12@naver.com>
2024-10-23 10:28:24 +0900

    update image list

--- ic/src/shared/assets/images/index.ts
+++ ic/src/shared/assets/images/index.ts
@@ -4,43 +4,9 @@ import ChestXray from './chest-xray.png';
 import BaseLogo from './base-logo.png';
 import DiagnosysAILogo from './diagnosysAI-logo.png';
 import MediScanAILogo from './mediScanAI-logo.png';
-import DiceBackgroundEffect from './dice-bg-effect.png';
-import GameTileBackground from './bg-gametile.png';
-import Dice1 from './1.png';
-import Dice2 from './2.png';
-import Dice3 from './3.png';
-import Dice4 from './4.png';
-import Dice5 from './5.png';
-import Dice6 from './6.png';
-import Airplane from './airplane.png';
-import Dice from './dice.png';
-import Star from './star.png';
-import CatLv1to2 from './CatCharacterlv1to2.png';
-import CatLv3to4 from './CatCharacterlv3to4.png';
-import CatLv5to6 from './CatCharacterlv5to6.png';
-import CatLv7to8 from './CatCharacterlv7to8.png';
-import CatLv9to10 from './CatCharacterlv9to10.png';
-import CatLv11to12 from './CatCharacterlv11to12.png';
-import CatLv13to14 from './CatCharacterlv13to14.png';
-import CatLv15to16 from './CatCharacterlv15to16.png';
-import CatLv17to18 from './CatCharacterlv17to18.png';
-import CatLv19to20 from './CatCharacterlv19to20.png';
-import DogLv1to2 from './DogCharacterlv1to2.png';
-import DogLv3to4 from './DogCharacterlv3to4.png';
-import DogLv5to6 from './DogCharacterlv5to6.png';
-import DogLv7to8 from './DogCharacterlv7to8.png';
-import DogLv9to10 from './DogCharacterlv9to10.png';
-import DogLv11to12 from './DogCharacterlv11to12.png';
-import DogLv13to14 from './DogCharacterlv13to14.png';
-import DogLv15to16 from './DogCharacterlv15to16.png';
-import DogLv17to18 from './DogCharacterlv17to18.png';
-import DogLv19to20 from './DogCharacterlv19to20.png';
 import SLSymbol from './sl_symbol.png';
-import MissionDice from './mission-dice.png';
 import RankingSLToken from './ranking-sl-token.png';
-import LotteryTicket from './lottery-ticket.png';
 import InviteFriend from './invite-friend.png';
-import Dice3D from './3d_dice.png';
 import Email from './email.png';
 import Twitter from './twitter.png';
 import LargeTwitter from './twitter_large.png';
@@ -57,27 +23,6 @@ import OkxLogo from './okx-logo.png';
 import TrustLogo from './trust-logo.png';
 import PrizeImage from './prizeImage.png';
 import TokenReward from './coin-reward.png';
-import Dices from './dices.png';
-import BGRPSGame from './bg-rpsgame.png';
-import BGSpinGame from './bg-spingame.png';
-import Spin from './spin.png';
-import SpinPin from './spin-pin.png';
-import SpinExample from './spin-example.png';
-import LosingTicket from './losing-ticket.png';
-import RPSGameExample from './RPSGame-example.png';
-import RPSGame from './RPSGame.png';
-import Rock from './rock.png';
-import Scissors from './scissors.png';
-import Paper from './paper.png';
-import SlotRock from './slot-rock.png';
-import SlotScissors from './slot-scissors.png';
-import SlotPaper from './slot-paper.png';
-import RockButton from './rock-button.png';
-import ScissorsButton from './scissors-button.png';
-import PaperButton from './paper-button.png';
-import RPSResultBG from './bg-RPS-result.png';
-import Victory from './victory.png';
-import Defeat from './defeat.png';
 
 const Images = {
   HorizontalLogo,
@@ -86,43 +31,9 @@ const Images = {
   BaseLogo,
   DiagnosysAILogo,
   MediScanAILogo,
-  DiceBackgroundEffect,
-  GameTileBackground,
-  Dice1,
-  Dice2,
-  Dice3,
-  Dice4,
-  Dice5,
-  Dice6,
-  Airplane,
-  Dice,
-  Star,
-  CatLv1to2,
-  CatLv3to4,
-  CatLv5to6,
-  CatLv7to8,
-  CatLv9to10,
-  CatLv11to12,
-  CatLv13to14,
-  CatLv15to16,
-  CatLv17to18,
-  CatLv19to20,
-  DogLv1to2,
-  DogLv3to4,
-  DogLv5to6,
-  DogLv7to8,
-  DogLv9to10,
-  DogLv11to12,
-  DogLv13to14,
-  DogLv15to16,
-  DogLv17to18,
-  DogLv19to20,
   SLSymbol,
-  MissionDice,
   RankingSLToken,
-  LotteryTicket,
   InviteFriend,
-  Dice3D,
   Email,
   Twitter,
   LargeTwitter,
@@ -139,27 +50,6 @@ const Images = {
   TrustLogo,
   PrizeImage,
   TokenReward,
-  Dices,
-  BGRPSGame,
-  BGSpinGame,
-  Spin,
-  SpinPin,
-  SpinExample,
-  LosingTicket,
-  RPSGameExample,
-  RPSGame,
-  Rock,
-  Scissors,
-  Paper,
-  SlotRock,
-  SlotScissors,
-  SlotPaper,
-  RockButton,
-  ScissorsButton,
-  PaperButton,
-  RPSResultBG,
-  Victory,
-  Defeat,
 };
 
 export default Images;
